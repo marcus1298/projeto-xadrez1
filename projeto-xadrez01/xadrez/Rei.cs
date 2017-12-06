472702a10e76021db546da1f70ada1508bc467a6
Marcus1298 <marcusviniciusiftm@gmail.com>
2017-12-06 10:40:27 -0200

    Implementando pecas

---
+++ projeto-xadrez01/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+using System;
+using xadrez;
+
+namespace xadrez{
+    class Rei : Peca {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+
+        public override string ToString(){
+        
+                return "R";
+            }
+        }
+    }
+
